b4e070451de8f268234def6936ccd3687db2b4e2
himanshu <royalhmj@gmail.com>
2023-04-09 00:11:30 +0530

    bounce loader

--- Frontend/src/Components/Signin.jsx
+++ Frontend/src/Components/Signin.jsx
@@ -1,44 +1,57 @@
-// import { useState } from "react";
+import { useState } from "react";
 import { useNavigate } from "react-router-dom";
-function Signin({data,setData}) {
-const forward = "https://sociomeetbackend.onrender.com";
+import BounceLoader from 'react-spinners/BounceLoader';
 
-const navigate = useNavigate();
- const handleSubmit = async(e)=>{
-  e.preventDefault();
-  const res = await fetch(`${forward}/api/login`,{
-    method : "POST",
-    headers:{
-      'Content-Type' : "application/json"
-    },
-    body:JSON.stringify({
-      ...data
-    })  
-  })
- console.log(res,'res');
- if(res.status===200){
-  const { token,user } = await res.json();
-  console.log(token,user,'res')
-  localStorage.setItem('user:token',token)
-  navigate('/');
- }
- else if(res.status===401){
-  alert('Invalid user or password')
- }
- else{
-  alert('server error')
- }
+function Signin({ data, setData }) {
+  const forward = "https://sociomeetbackend.onrender.com";
 
-}
+  const navigate = useNavigate();
+  const [loading, SetLoading] = useState(false);
 
-    return(
-      <form className="signin_form" onSubmit={(e)=>handleSubmit(e)}>
-     <input className="input" type="email"  placeholder="Enter your email" value={data.email} id="email" onChange={(e)=>setData({...data, email:e.target.value})} required={true}/> 
-     <input className="input" type="password" id="password" placeholder="Enter Password" value={data.password}  onChange={(e)=>setData({...data, password:e.target.value})} required={true}/> 
-      <button className="button_auth" type="submit"> LOGIN</button>   
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    SetLoading(true);
+    const res = await fetch(`${forward}/api/login`, {
+      method: "POST",
+      headers: {
+        'Content-Type': "application/json"
+      },
+      body: JSON.stringify({
+        ...data
+      })
+    })
+    SetLoading(false);
+    console.log(res, 'res');
+    if (res.status === 200) {
+      const { token, user } = await res.json();
+      console.log(token, user, 'res')
+      localStorage.setItem('user:token', token)
+      navigate('/');
+    }
+    else if (res.status === 401) {
+      alert('Invalid user or password')
+    }
+    else {
+      alert('server error')
+    }
+
+  }
+
+  return (
+    <div>{
+      loading ?
+        <BounceLoader />
+        :
+
+        <form className="signin_form" onSubmit={(e) => handleSubmit(e)}>
+          <input className="input" type="email" placeholder="Enter your email" value={data.email} id="email" onChange={(e) => setData({ ...data, email: e.target.value })} required={true} />
+          <input className="input" type="password" id="password" placeholder="Enter Password" value={data.password} onChange={(e) => setData({ ...data, password: e.target.value })} required={true} />
+          <button className="button_auth" type="submit"> LOGIN</button>
 
         </form>
-    )
+    }
+    </div>
+  )
 
 }
 export default Signin;
